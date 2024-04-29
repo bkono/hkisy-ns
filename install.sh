70232f13c36dddcff187dd6ba210a09f3deb5c29
Bryan Konowitz <bryan@kono.sh>
2024-04-29 10:09:42 -0700

    fix: update install to use public repo

--- install.sh
+++ install.sh
@@ -11,15 +11,15 @@ os=$(uname -s)
 
 if [ -x "$(command -v curl)" ]; then
   echo "curl present, using it for retrieving hkisy"
-  tag=$(curl --silent "https://api.github.com/repos/bkono/hkisy/releases/latest" | grep '"tag_name":' | cut -d'"' -f4)
+  tag=$(curl --silent "https://api.github.com/repos/bkono/hkisy-ns/releases/latest" | grep '"tag_name":' | cut -d'"' -f4)
   downloadCmd="curl -L -o"
 elif [ -x "$(command -v wget)" ]; then
   echo "wget present, using it for retrieving hkisy"
-  tag=$(wget -q -O - https://api.github.com/repos/bkono/hkisy/releases/latest | grep '"tag_name":' | cut -d'"' -f4)
+  tag=$(wget -q -O - https://api.github.com/repos/bkono/hkisy-ns/releases/latest | grep '"tag_name":' | cut -d'"' -f4)
   downloadCmd="wget -O"
 fi
 
-release="https://github.com/bkono/hkisy/releases/download/$tag/hkisy-$tag-$os-amd64.tar.gz"
+release="https://github.com/bkono/hkisy-ns/releases/download/$tag/hkisy-$tag-$os-amd64.tar.gz"
 tarball="hkisy.tar.gz"
 
 echo "downloading release: $release"
